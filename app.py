d890e4b70f3060dc3c74fe6bf6339b0c259bc8a2
EC2 Default User <ec2-user@ip-10-0-13-193.ec2.internal>
2023-09-23 22:49:12 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/add")
+def add():
+    val1 = request.args.get('val1')
+    val2 = request.args.get('val2')
+    sum = int(val1) + int(val2)
+    return str(sum)
+
+
+if __name__ == '__main__':
+    app.run(threaded=True,host='0.0.0.0',port=80)
